6d347a0e3682d0e4fe5b49a68d61d5b0b6e535c4
zclmac <zclmac@zclmacdeMac.local>
2019-11-22 00:15:22 -0800

    创建公告窗体

---
+++ jcapp/jcapp/Notice/NoticeViewController.h
@@ -0,0 +1,23 @@
+//
+//  NoticeViewController.h
+//  jcapp
+//
+//  Created by zclmac on 2019/11/21.
+//  Copyright © 2019 midtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NoticeViewController : UIViewController
+
+
+// 添加数据源
+@property (strong,nonatomic) NSMutableArray *listOfMovies;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
+

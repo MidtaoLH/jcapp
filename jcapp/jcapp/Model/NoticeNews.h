4d2527c2a9cb49027db8b9990d3a77a22aceefcf
zclmac <zclmac@zclmacdeMac.local>
2019-11-27 00:54:24 -0800

    提交公告

---
+++ jcapp/jcapp/Model/NoticeNews.h
@@ -0,0 +1,14 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface NoticeNews : NSObject
+/** 名称 */
+@property (copy, nonatomic) NSString *NewsTheme;
+
+/** 年龄 */
+@property (copy, nonatomic) NSString *NewsContent;
+
+
+@end
+ 

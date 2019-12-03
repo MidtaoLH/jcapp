aed91599a65cf0836272df65af3ab40410449d06
zhaodan <zhaodan@zhaodandeMac.local>
2019-12-02 22:36:46 -0800

    追加

---
+++ jcapp/jcapp/Model/UserLogin.h
@@ -0,0 +1,29 @@
+//
+//  UserLogin.h
+//  jcapp
+//
+//  Created by zhaodan on 2019/12/2.
+//  Copyright © 2019 midtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserLogin : NSObject
+
+
+/** dengluflag */
+@property (copy, nonatomic) NSString *flag;
+
+/** id */
+@property (copy, nonatomic) NSString *id;
+
+/** yonghuming */
+@property (copy, nonatomic) NSString *code;
+
+/** mingcheng */
+@property (copy, nonatomic) NSString *name;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ jcapp/jcapp/Model/UserLogin.m
@@ -0,0 +1,15 @@
+//
+//  User.m
+//  jcapp
+//
+//  Created by zclmac on 2019/11/24.
+//  Copyright © 2019 midtao. All rights reserved.
+//
+
+#import "UserLogin.h"
+
+
+@implementation UserLogin
+
+
+@end

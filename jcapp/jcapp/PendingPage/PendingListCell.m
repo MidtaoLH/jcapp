9fc0f2a6df68dcf34c1fbfb9d2e7a12496ffd0fe
lh <lh@lhdeMac.local>
2019-12-05 01:13:58 -0800

    【待审批】

--- jcapp/jcapp/PendingPage/PendingListCell.m
+++ jcapp/jcapp/PendingPage/PendingListCell.m
@@ -116,7 +116,7 @@
     self.beignDateLable.frame = CGRectMake(2*kMargin+imageWH, 2*txtH+3*kMargin, width - leaveDateWidth - kMargin - imageWH, txtH);
     self.endDateLable.frame = CGRectMake(2*kMargin+imageWH, 3*txtH+4*kMargin, width - leaveDateWidth - kMargin - imageWH, txtH);
     self.pendingStatusLable.frame = CGRectMake(2*kMargin+imageWH, 4*txtH+5*kMargin, width - leaveDateWidth - kMargin - imageWH, txtH);
-    self.textLabel.frame = CGRectMake(3*kMargin+imageWH,kMargin, imageWH*2, txtH);
+    self.textLabel.frame = CGRectMake(2*kMargin+imageWH,kMargin, imageWH*2, txtH);
     
     self.imageView.layer.masksToBounds = YES;
     self.imageView.layer.cornerRadius = imageWH * 0.5;

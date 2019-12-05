dcef73e8745d0726e72675877e79e0342216c71c
lh <lh@lhdeMac.local>
2019-12-04 17:17:23 -0800

    【待申请】

--- jcapp/jcapp/PendingPage/PendingViewController.m
+++ jcapp/jcapp/PendingPage/PendingViewController.m
@@ -18,17 +18,34 @@ static NSString * identifier = @"PendingListCell";
 @end
 
 @implementation PendingViewController
-
+NSInteger currentPage;
 @synthesize listOfMovies;
 
 - (void)viewDidLoad {
     
     self.title = @"待审批记录";
-    
+    //e注册自定义 cell
+    [_NewTableView registerClass:[PendingListCell class] forCellReuseIdentifier:identifier];
+    _NewTableView.rowHeight = 150;
+    CGFloat headimageX = self.view.frame.size.width;
+    CGFloat headimageY = self.view.frame.size.height;
+    CGFloat headimageW = self.view.frame.size.width*1.104;
+    CGFloat headimageH =  self.view.frame.size.height;
+    self.NewTableView.frame = CGRectMake(0, 0, headimageW, headimageH);
+    currentPage=1;
+    [self LoadDate];
+    [super viewDidLoad];
+}
+
+-(void)LoadDate
+{
+    NSUserDefaults * defaults = [NSUserDefaults standardUserDefaults];
+    NSString *user = [defaults objectForKey:@"username"];
+    NSString *userid = [defaults objectForKey:@"userid"];
     //设置需要访问的ws和传入参数
-    
-    NSString *strURL = [NSString stringWithFormat:@"http://47.94.85.101:8095/AppWebService.asmx/GetLeaveList"];
-    
+    // code, string userID, string menuID
+    NSString *currentPagestr = [NSString stringWithFormat: @"%ld", (long)currentPage];
+    NSString *strURL = [NSString stringWithFormat:@"http://47.94.85.101:8095/AppWebService.asmx/GetPendingInfo?pasgeIndex=%@&pageSize=%@&code=%@&userID=%@&menuID=%@",currentPagestr,@"10",@"19",@"19",@"4"];
     NSURL *url = [NSURL URLWithString:strURL];
     //进行请求
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
@@ -36,50 +53,21 @@ static NSString * identifier = @"PendingListCell";
     NSURLConnection *connection = [[NSURLConnection alloc]
                                    initWithRequest:request
                                    delegate:self];
-    //e注册自定义 cell
-    [_NewTableView registerClass:[PendingListCell class] forCellReuseIdentifier:identifier];
-    _NewTableView.rowHeight = 150;
-    
-    [super viewDidLoad];
-    
-    
-    
-    NSLog(@"%@",@"viewDidLoad-end");
 }
-
-
 //系统自带方法调用ws后进入将gbk转为utf-8如果确认是utf-8可以不转，因为ios只认utf-8
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    NSLog(@"%@",@"connection1-begin");
-    //upateData = [[NSData alloc] initWithData:data];
-    //默认对于中文的支持不好
-    //   NSStringEncoding enc = CFStringConvertEncodingToNSStringEncoding(kCFStringEncodingGB_18030_2000);
-    //   NSString *gbkNSString = [[NSString alloc] initWithData:data encoding: enc];
-    //如果是非UTF－8  NSXMLParser会报错。
-    //   xmlString = [[NSString alloc] initWithString:[gbkNSString stringByReplacingOccurrencesOfString:@"<?xml version=\"1.0\" encoding=\"gbk\"?>"
-    //                                                                                       withString:@"<?xml version=\"1.0\" encoding=\"utf-8\"?>"]];
-    
     xmlString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    
-    NSLog(@"%@", @"kaishidayin");
-    NSLog(@"%@", xmlString);
-    
     // 字符串截取
     NSRange startRange = [xmlString rangeOfString:@"<string xmlns=\"http://tempuri.org/\">"];
     NSRange endRagne = [xmlString rangeOfString:@"</string>"];
     NSRange reusltRagne = NSMakeRange(startRange.location + startRange.length, endRagne.location - startRange.location - startRange.length);
     NSString *resultString = [xmlString substringWithRange:reusltRagne];
-    
-    NSLog(@"%@", resultString);
-    
     NSString *requestTmp = [NSString stringWithString:resultString];
     NSData *resData = [[NSData alloc] initWithData:[requestTmp dataUsingEncoding:NSUTF8StringEncoding]];
     
-    
     NSMutableDictionary *resultDic = [NSJSONSerialization JSONObjectWithData:resData options:NSJSONReadingMutableLeaves error:nil];
     listOfMovies = [Pending mj_objectArrayWithKeyValuesArray:resultDic];
-    
-    NSLog(@"%@",@"connection1-end");
+    //[self.listOfMovies addObjectsFromArray:self.listMovies];
 }
 
 //弹出消息框
@@ -92,27 +80,19 @@ static NSString * identifier = @"PendingListCell";
                                cancelButtonTitle:@"OK"
                                otherButtonTitles:nil];
     [errorAlert show];
-    NSLog(@"%@",@"connection2-end");
 }
 
 //解析返回的xml系统自带方法不需要h中声明
 - (void) connectionDidFinishLoading: (NSURLConnection*) connection {
-    
-    NSLog(@"%@", @"kaishijiex");    //开始解析XML
-    
     NSXMLParser *ipParser = [[NSXMLParser alloc] initWithData:[xmlString dataUsingEncoding:NSUTF8StringEncoding]];
     ipParser.delegate = self;
     [ipParser parse];
-    NSLog(@"%@",@"connectionDidFinishLoading-end");
-    
     [self.NewTableView reloadData];
 }
 
 //解析xml回调方法
 - (void)parserDidStartDocument:(NSXMLParser *)parser {
     info = [[NSMutableDictionary alloc] initWithCapacity: 1];
-    
-    NSLog(@"%@",@"parserDidStartDocument-end");
 }
 
 //回调方法出错弹框
@@ -124,7 +104,6 @@ static NSString * identifier = @"PendingListCell";
                                cancelButtonTitle:@"OK"
                                otherButtonTitles:nil];
     [errorAlert show];
-    NSLog(@"%@",@"parser-end");
 }
 
 //解析返回xml的节点elementName
@@ -132,17 +111,11 @@ static NSString * identifier = @"PendingListCell";
   namespaceURI:(NSString *)namespaceURI
  qualifiedName:(NSString *)qualifiedName
     attributes:(NSDictionary *)attributeDict  {
-    NSLog(@"value2: %@\n", elementName);
-    //NSLog(@"%@", @"jiedian1");    //设置标记查看解析到哪个节点
     currentTagName = elementName;
-    
-    NSLog(@"%@",@"parser2-end");
 }
 
 //取得我们需要的节点的数据
 - (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string {
-    
-    NSLog(@"%@",@"parser3-begin");
     
 }
 - (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName
@@ -153,23 +126,16 @@ static NSString * identifier = @"PendingListCell";
 
 //循环解析d节点
 - (void)parserDidEndDocument:(NSXMLParser *)parser {
-    
-    NSLog(@"%@",@"parserDidEndDocument-begin");
-    
     NSMutableString *outstring = [[NSMutableString alloc] initWithCapacity: 1];
     for (id key in info) {
         [outstring appendFormat: @"%@: %@\n", key, [info objectForKey:key]];
     }
-    
-    //[outstring release];
-    //[xmlString release];
 }
 
 
 //有多少组
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    NSLog(@"%@",@"numberOfSectionsInTableView-begin");
     // 默认有些行，请删除或注 释 #warning Potentially incomplete method implementation.
     // 这里是返回的节点数，如果是简单的一组数据，此处返回1，如果有多个节点，就返回节点 数
     return 1;
@@ -181,45 +147,61 @@ static NSString * identifier = @"PendingListCell";
 {
     // 默认有此行，请删除或注 释 #warning Incomplete method implementation.
     // 这里是返回节点的行数
-    NSLog(@"%@",@"tableView-begin");
     return self.listOfMovies.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
-    //  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID"];
-    //  if (cell == nil){
-    //      cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cellID"];
-    //  }
-    // 大家还记得，之前让你们设置的Cell Identifier 的 值，一定要与前面设置的值一样，不然数据会显示不出来
     PendingListCell * cell = [self.NewTableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
-    
-    // LeaveListCell * cell =[tableView dequeueReusableCellWithIdentifier:identifier];
-    
-    cell.pendinglistitem =self.listOfMovies[indexPath.row];//取出数据元素
-    
-    //  LeaveListModel *m =self.listOfMovies[indexPath.row];//取出数据元素
-    
-    //  cell.textLabel.text = m.LeaveVersion;
-    
-    //  cell.detailTextLabel.text = m.LeaveApplyCode;
-    
-    
-    
+    if(cell == nil)
+    {
+        if([indexPath row] == [self.listOfMovies count])
+        {
+            //新建一个单元格, 并且将其样式调整成我们需要的样子.
+            cell=[[UITableViewCell alloc] initWithFrame:CGRectZero
+                  
+                                        reuseIdentifier:@"LoadMoreIdentifier"];
+            cell.font = [UIFont boldSystemFontOfSize:13];
+            cell.textLabel.text = @"读取更多...";
+        }
+    }
+    else
+    {
+        cell.pendinglistitem =self.listOfMovies[indexPath.row];//取出数据元素
+    }
     return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if([indexPath row] == [self.listOfMovies count])
+    {
+        UITableViewCell *loadMoreCell=[tableView cellForRowAtIndexPath:indexPath];
+        loadMoreCell.textLabel.text=@"正在读取更多信息 …";
+        [self performSelectorInBackground:@selector(loadMore) withObject:nil];
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+        return;
+    }
+    else
+    {
+        //其它单元格的事件
+    }
+}
+-(void)loadMore
+{
+    //当你按下这个按钮的时候, 意味着你需要看下一页了, 因此当前页码加1
+    currentPage++;
+    [self LoadDate];//通过调用GetRecord方法, 将数据取出.
 }
 
 -(NSString*)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
     // 返回顶部标题
-    NSLog(@"%@",@"tableView2-begin");
-    return @"请假记录";
+    return @"待审批记录";
 }
 
 -(NSString*)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section
 {
-    NSLog(@"%@",@"tableView3-begin");
     // 返回底部文字
     return @"中道益通";
 }

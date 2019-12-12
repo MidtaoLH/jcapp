7676ce1a8505dad6cd18d22193adeccad464c7e9
zhaodan <zhaodan@zhaodandeMac.local>
2019-12-12 04:50:15 -0800

    add

---
+++ jcapp/jcapp/Model/Emp.m
@@ -0,0 +1,16 @@
+//
+//  Way.m
+//  jcapp
+//
+//  Created by zhaodan on 2019/12/3.
+//  Copyright © 2019 midtao. All rights reserved.
+//
+
+#import "Emp.h"
+
+
+
+@implementation Emp
+
+
+@end
---
+++ jcapp/jcapp/Model/Group.m
@@ -0,0 +1,16 @@
+//
+//  Way.m
+//  jcapp
+//
+//  Created by zhaodan on 2019/12/3.
+//  Copyright © 2019 midtao. All rights reserved.
+//
+
+#import "Group.h"
+
+
+
+@implementation Group
+
+
+@end

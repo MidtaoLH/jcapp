87cc55c958d4eaedea379e603412d65ab29a6ec0
zclmac <zclmac@zclmacdeMac.local>
2020-01-02 17:34:17 +0800

    tijiao

---
+++ jcapp/jcapp/Model/ExamineHead.m
@@ -0,0 +1,13 @@
+//
+//  ExamineHead.m
+//  jcapp
+//
+//  Created by zclmac on 2020/1/2.
+//  Copyright © 2020 midtao. All rights reserved.
+//
+
+#import "ExamineHead.h"
+
+@implementation ExamineHead
+
+@end
